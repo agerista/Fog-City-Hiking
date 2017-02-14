3192143d616320b12532639f6f8bffc6bcf3a755
agerista <amandacrawfordcodes@gmail.com>
2017-02-14 22:47:41 +0000

    weather working

--- weather.py
+++ weather.py
@@ -7,26 +7,25 @@ import forecastio
 api_key = os.environ["DARK_KEY"]
 
 
-# def weather_forecast():
-#     """Get the weather"""
-
+def weather_forecast():
+    """Get the weather"""
 
     # Cap is 1,000 calls per day 
-    # coordinates = db.session.query(Park.latitude, Park.longitude).distinct().first()
+    # db.session.query(Park.latitude, Park.longitude).distinct().first()
+    coordinates = [(37.7749, -122.4194), (37.8044, -122.2711), (37.8716, -122.2727), (38.0834, -122.7633), (37.6138, -122.4869)]
 
-    # for coordinate in coordinates:
+    for coordinate in coordinates:
 
-    #     type(coordinate)
-    #     print coordinate
+        lat = coordinate[0]
+        lng = coordinate[1]
 
-    #     lat = coordinate[0]
-    #     lng = coordinate[1]
+        weather = forecastio.load_forecast(api_key, lat, lng)
+        hour = weather.hourly()
+        summary = hour.summary
+        icon = hour.icon
+        print summary, icon
 
-weather = forecastio.load_forecast(api_key,50.0,10.0)
-        # weather = forecastio.load_forecast(api_key, lat, lng)
-        # weather = forecastio.load_forecast(api_key, coordinate[0], coordinate[1])
-
-    # return weather
+    return summary, icon
 
 
 if __name__ == "__main__":
